9f240fb83cb1acce858ed45c7133dfdab854712e
Elodie-mbaye <9992@holbertonstudents.com>
2025-04-01 20:09:11 +0200

    Create a function updateStudentGradeByCity that returns an array of students for a specific city with their new grade

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,10 @@
+/*eslint-disable*/
+export default function updateStudentGradeByCity(students, city, newGrades) {
+    return students
+      .filter((student) => student.location === city)
+      .map((student) => {
+        const newGrade = newGrades.find((grade) => grade.studentId === student.id);
+        const grade = newGrade ? newGrade.grade : 'N/A';
+        return { ...student, grade };
+      });
+  }
