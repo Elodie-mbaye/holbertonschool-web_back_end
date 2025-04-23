b5b14075910009b48b5851a1627a11b2d939070d
Elodie-mbaye <9992@holbertonstudents.com>
2025-04-23 17:51:41 +0200

    return values with the appropriate types

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""Module provides a function that returns a tuple """
+
+
+from typing import Iterable, List, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+
+    Args:
+    Lst: Iterable[sequence]
+
+    Returns:
+    A list of tuples
+    """
+
+    return [(i, len(i)) for i in lst]
