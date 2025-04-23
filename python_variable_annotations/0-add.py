258096e02829baf15522afbb87170d06f6f6ea76
Elodie-mbaye <9992@holbertonstudents.com>
2025-04-23 16:17:14 +0200

    add that takes a float a and a float b as arguments and returns their sum as a float.

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module providing a fonction to add two float"""
 
+
 def add(a: float, b: float) -> float:
     """Add two float numbers and return their sum"""
     return a + b
