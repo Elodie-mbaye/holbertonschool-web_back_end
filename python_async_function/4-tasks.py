792b3257af2bb490b0a03ae7bdc301b1d23a189f
Elodie-mbaye <9992@holbertonstudents.com>
2025-04-26 22:40:51 +0200

    task 4

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Function that execute multiple tasks using task_wait_random"""
+
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """return the results of all delay (float values)"""
+    delay = [task_wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*delay)
+    return sorted(results)
