30676617a66174b4116bd7e8cd1378284e45e36e
Elodie-mbaye <9992@holbertonstudents.com>
2025-05-21 18:54:23 +0200

    the file

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,12 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}\n`);
+process.stdin.on('readable', () => {
+  const word = process.stdin.read();
+  if (word) {
+    process.stdout.write(`Your name is: ${word.toString()}`);
+  }
 });
 
 process.stdin.on('end', () => {
-    process.stdout.write('This important software is now closing\n');
+  console.log('This important software is now closing');
 });
